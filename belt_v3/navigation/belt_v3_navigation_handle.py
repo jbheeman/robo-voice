e17aeec5de549734e3e23f650b6c9cdbd29fba2b
Lionel-Ip <lionel.hy.ip@gmail.com>
2026-07-17 14:19:17 -0700

    Make navigation accept uppercase versions belt_v3_navigation_handle.py

--- belt_v3/navigation/belt_v3_navigation_handle.py
+++ belt_v3/navigation/belt_v3_navigation_handle.py
@@ -2,7 +2,7 @@ from belt_v3_valid_navigation import VALID_LOCATIONS
 
 def navigation_handle(navigation_list : list):
     for nav in navigation_list:
-        if nav not in VALID_LOCATIONS:
+        if nav.lower() not in VALID_LOCATIONS:
             navigation_list.remove(nav)
             
     print(f"Navigation handle: {navigation_list}")
