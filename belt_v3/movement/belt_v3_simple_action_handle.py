2ac01b795b9e7637af7ecd1eae92dd2b6ac68392
Lionel-Ip <lionel.hy.ip@gmail.com>
2026-07-17 14:19:58 -0700

    Make navigation accept uppercase versions belt_v3_simple_action_handle.py

--- belt_v3/movement/belt_v3_simple_action_handle.py
+++ belt_v3/movement/belt_v3_simple_action_handle.py
@@ -2,7 +2,7 @@ from belt_v3_valid_movements import VALID_MOVEMENTS
 
 def simple_action_handle(simple_action_list : list):
     for act in simple_action_list:
-        if act not in VALID_MOVEMENTS:
+        if act.lower() not in VALID_MOVEMENTS:
             simple_action_list.remove(act)
             
     print(f"Simple action handle {simple_action_list}")
